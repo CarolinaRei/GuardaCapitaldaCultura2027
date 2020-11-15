2487f3c06fffd81a1e82f6b1d4a26f6b311a95d4
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-11-15 16:22:34 +0000

    Adionadas todas as paginas que compoem os locais onde vao decorrer eventos

--- GuardaCapitaldaCultura2027/Controllers/HomeController.cs
+++ GuardaCapitaldaCultura2027/Controllers/HomeController.cs
@@ -27,8 +27,41 @@ namespace GuardaCapitaldaCultura2027.Controllers
         {
             return View();
         }
+       public IActionResult Aguiar()
+        {
+            return View();
+        }   
+        public IActionResult Almeida()
+        {
+            return View();
+        }
+        public  IActionResult Belmonte()
+        {
+            return View();
+        }
+
+        public IActionResult Celorico()
+        {
+            return View();
+        }
 
         public IActionResult Covilha()
+        {
+            return View();
+        }
+        public IActionResult FCR()
+        {
+            return View();
+        }
+        public IActionResult Fornos()
+        {
+            return View();
+        }
+        public IActionResult Fundao()
+        {
+            return View();
+        }
+        public IActionResult Gouveia()
         {
             return View();
         }
@@ -36,11 +69,37 @@ namespace GuardaCapitaldaCultura2027.Controllers
         {
             return View();
         }
+        public IActionResult Manteigas()
+        {
+            return View();
+        }
+        public IActionResult Meda()
+        {
+            return View();
+        }
+        public IActionResult Pinhel()
+        {
+            return View();
+        }
+        public IActionResult Sabugal()
+        {
+            return View();
+        }
+
         public IActionResult Seia()
         {
             return View();
         }
 
+        public IActionResult Trancoso()
+        {
+            return View();
+        }
+
+        public IActionResult VNFC()
+        {
+            return View();
+        }
         public IActionResult Erro()
         {
             return View();
