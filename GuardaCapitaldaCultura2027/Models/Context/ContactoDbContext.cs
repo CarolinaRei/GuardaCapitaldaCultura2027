a2e3d6004936ff7c9fe7c24fc239f09c660aabee
Vagner Bom Jesus <vagneripg@gmail.com>
2020-11-22 16:40:27 +0000

    Criar o ContactoDbContext

---
+++ GuardaCapitaldaCultura2027/Models/Context/ContactoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCapitaldaCultura2027.Models.Context
+{
+    public class ContactoDbContext : DbContext
+    {
+        public ContactoDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Contacto> Contactos { get; set; }
+    }
+}
