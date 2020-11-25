7ca6923b5a669fa17887aff375dbd198a569362f
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-11-25 21:15:51 +0000

    Criação do controlador Guarda Modificação da View

---
+++ GuardaCapitaldaCultura2027/Controllers/GuardaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuardaCapitaldaCultura2027.Controllers
+{
+    public class GuardaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
