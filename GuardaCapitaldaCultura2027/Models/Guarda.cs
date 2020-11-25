ed2e32afe3d0f46030d65b81b5c3386c2c2b3cc4
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-11-25 22:13:26 +0000

    Scaffolding da pagina da Guarda Erro no Model

---
+++ GuardaCapitaldaCultura2027/Models/Guarda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCapitaldaCultura2027.Models
+{
+    public class Guarda
+    {
+        public int GuardaId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+        
+        public int Ano { get; set; }
+    }
+}
