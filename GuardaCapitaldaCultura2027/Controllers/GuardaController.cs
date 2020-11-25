6ae06634b03d8e12a33ddf02949d603f69571540
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-11-25 22:40:03 +0000

    Scaffolding Completo View já lista

--- GuardaCapitaldaCultura2027/Controllers/GuardaController.cs
+++ GuardaCapitaldaCultura2027/Controllers/GuardaController.cs
@@ -8,9 +8,14 @@ namespace GuardaCapitaldaCultura2027.Controllers
 {
     public class GuardaController : Controller
     {
+        static IList<Models.Guarda> GuardaList = new List<Models.Guarda>{
+                new Models.Guarda() { GuardaId = 1,    Name = "Guarda 2020"  , Ano = 2020 } ,
+                new Models.Guarda() { GuardaId = 2,    Name = "Cidade Natal" , Ano = 2020 } ,
+                new Models.Guarda() { GuardaId = 3,    Name = "Guarda Futuro"  , Ano = 2021 }
+            };
         public IActionResult Index()
         {
-            return View();
+            return View(GuardaList);
         }
     }
 }
