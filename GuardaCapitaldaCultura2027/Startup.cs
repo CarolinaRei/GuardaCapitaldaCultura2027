a95d164488288db0ef6564139ce27cca60df962b
Vagner Bom Jesus <vagneripg@gmail.com>
2020-11-30 16:34:26 +0000

    Atualisacao do design do Contact/Index

--- GuardaCapitaldaCultura2027/Startup.cs
+++ GuardaCapitaldaCultura2027/Startup.cs
@@ -68,7 +68,7 @@ namespace GuardaCapitaldaCultura2027
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Contactos}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
