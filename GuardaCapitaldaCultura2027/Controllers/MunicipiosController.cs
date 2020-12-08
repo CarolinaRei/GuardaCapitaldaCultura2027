d6526959375c6c87f34a4e094adb2096b7e339ce
Vagner Bom Jesus <vagneripg@gmail.com>
2020-12-08 15:53:30 +0000

    Criação de Controlador Municipio

---
+++ GuardaCapitaldaCultura2027/Controllers/MunicipiosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GuardaCapitaldaCultura2027.Models;
+using GuardaCapitaldaCultura2027.Models.Context;
+
+namespace GuardaCapitaldaCultura2027.Controllers
+{
+    public class MunicipiosController : Controller
+    {
+        private readonly GuardaEventosBdContext _context;
+
+        public MunicipiosController(GuardaEventosBdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Municipios
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Municipios.ToListAsync());
+        }
+
+        // GET: Municipios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var municipio = await _context.Municipios
+                .FirstOrDefaultAsync(m => m.MunicipioId == id);
+            if (municipio == null)
+            {
+                return NotFound();
+            }
+
+            return View(municipio);
+        }
+
+        // GET: Municipios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Municipios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MunicipioId,Nome,Descricao,Imagem")] Municipio municipio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(municipio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(municipio);
+        }
+
+        // GET: Municipios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var municipio = await _context.Municipios.FindAsync(id);
+            if (municipio == null)
+            {
+                return NotFound();
+            }
+            return View(municipio);
+        }
+
+        // POST: Municipios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MunicipioId,Nome,Descricao,Imagem")] Municipio municipio)
+        {
+            if (id != municipio.MunicipioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(municipio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MunicipioExists(municipio.MunicipioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(municipio);
+        }
+
+        // GET: Municipios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var municipio = await _context.Municipios
+                .FirstOrDefaultAsync(m => m.MunicipioId == id);
+            if (municipio == null)
+            {
+                return NotFound();
+            }
+
+            return View(municipio);
+        }
+
+        // POST: Municipios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var municipio = await _context.Municipios.FindAsync(id);
+            _context.Municipios.Remove(municipio);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MunicipioExists(int id)
+        {
+            return _context.Municipios.Any(e => e.MunicipioId == id);
+        }
+    }
+}
