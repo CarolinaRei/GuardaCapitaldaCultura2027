b899322a99d43125c868d6c0cd6a75fd7209184a
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-12-10 09:11:52 +0000

    Paginação

---
+++ GuardaCapitaldaCultura2027/Models/IEventosRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCapitaldaCultura2027.Models
+{
+    public class IEventosRepository
+    {
+        public IEnumerable<Evento> Evento { get; }
+    }
+}
