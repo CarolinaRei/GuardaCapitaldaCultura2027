d0136d72d5b8bca987e6434f3382ec86d53e311d
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-12-11 10:16:55 +0000

    Edição de eventos (correção de erros)

---
+++ GuardaCapitaldaCultura2027/Controllers/Reserva.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuardaCapitaldaCultura2027.Controllers
+{
+    public class Reserva : Controller
+    {
+        // GET: Reserva
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Reserva/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reserva/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reserva/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reserva/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reserva/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reserva/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reserva/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GuardaCapitaldaCultura2027/Models/Reserva.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCapitaldaCultura2027.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int EventosId { get; set; }
+
+
+        [Display(Name = "Feedback")]
+       ")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 20 caracteres")]
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Por favor, insira a Descrição")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "A descrição deve ter entre 10 e 500 caracteres")]
+
+    }
+}
