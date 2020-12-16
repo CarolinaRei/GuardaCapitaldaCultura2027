ebd6854fe6b4b7757d0c3df4cf97c0f519bd30df
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-12-16 22:52:51 +0000

    Adicionado Turista nas reservas

--- GuardaCapitaldaCultura2027/Models/Reservas.cs
+++ GuardaCapitaldaCultura2027/Models/Reservas.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,13 @@ namespace GuardaCapitaldaCultura2027.Models
         public int ReservarId { get; set; }
 
         //TuristaID
+        [ForeignKey("FK_TuristaId")]
+        [Display(Name = "Turista")]
+        public int TuristaId { get; set; }
+
+       // public Turista Turista { get; set; }
+
+       // public ICollection<Turista> Turistas { get; set; }
 
         //EventoID
 
