ad35eeab15aac97b47f452e78fc3304d9ecb2d22
CarolinaRei <61904160+CarolinaRei@users.noreply.github.com>
2020-12-17 11:56:07 +0000

    Correções dos Eventos

---
+++ GuardaCapitaldaCultura2027/Controllers/EventosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GuardaCapitaldaCultura2027.Models;
+using GuardaCapitaldaCultura2027.Models.Context;
+
+namespace GuardaCapitaldaCultura2027.Controllers
+{
+    public class EventosController : Controller
+    {
+        private readonly GuardaEventosBdContext _context;
+
+        public EventosController(GuardaEventosBdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Eventos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Eventos.ToListAsync());
+        }
+
+        // GET: Eventos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos
+                .FirstOrDefaultAsync(m => m.EventosId == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        // GET: Eventos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Eventos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventosId,Name,Descricao,Data_realizacao,Lotacao_max,Lotacao_Ocupada")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(evento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evento);
+        }
+
+        // GET: Eventos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Eventos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventosId,Name,Descricao,Data_realizacao,Lotacao_max,Lotacao_Ocupada")] Evento evento)
+        {
+            if (id != evento.EventosId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(evento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventoExists(evento.EventosId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evento);
+        }
+
+        // GET: Eventos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Eventos
+                .FirstOrDefaultAsync(m => m.EventosId == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        // POST: Eventos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var evento = await _context.Eventos.FindAsync(id);
+            _context.Eventos.Remove(evento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventoExists(int id)
+        {
+            return _context.Eventos.Any(e => e.EventosId == id);
+        }
+    }
+}
--- GuardaCapitaldaCultura2027/Models/Context/GuardaEventosBdContext.cs
+++ GuardaCapitaldaCultura2027/Models/Context/GuardaEventosBdContext.cs
@@ -10,11 +10,10 @@ namespace GuardaCapitaldaCultura2027.Models.Context
     public class GuardaEventosBdContext : DbContext
     {
         public GuardaEventosBdContext(DbContextOptions options) : base(options) { }
-        //public DbSet<GuardaCapitaldaCultura2027.Models.Evento> Evento { get; set; }
 
         public DbSet<Contacto> Contactos { get; set; }
 
-        //public DbSet<Evento> Evento { get; set; }
+        public DbSet<Evento> Eventos { get; set; }
 
 
         public DbSet<Muicipio> Muicipios { get; set; }
