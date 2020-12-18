bcf2027294f51a08e491d0437e878b404b8ebad9
Vagner Bom Jesus <vagneripg@gmail.com>
2020-12-18 08:55:19 +0000

    criar modelo Lugar Evento

---
+++ GuardaCapitaldaCultura2027/Models/LugarEveto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaCapitaldaCultura2027.Models
+{
+    public class LugarEveto
+    {
+        public int LugarEveto_id { get; set; }
+
+        public bool Oucupado { get; set; }
+
+        public string NumeroCadeira { get; set; }
+
+
+
+    }
+}
