9afc4e174840fd22276262c2700526fbe1600166
AfonsoAntunes <afonsorgantunes@gmail.com>
2020-12-18 10:02:02 +0000

    Remodelação do Módulo do Turista
    Incompleto - Não está a conectar à BD

---
+++ GuardaCapitaldaCultura2027/Migrations/20201218094802_Turistas.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuardaCapitaldaCultura2027.Migrations
+{
+    public partial class Turistas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Turista",
+                table: "Turista");
+
+            migrationBuilder.RenameTable(
+                name: "Turista",
+                newName: "Turistas");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Turistas",
+                table: "Turistas",
+                column: "TuristaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Turistas",
+                table: "Turistas");
+
+            migrationBuilder.RenameTable(
+                name: "Turistas",
+                newName: "Turista");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Turista",
+                table: "Turista",
+                column: "TuristaId");
+        }
+    }
+}
--- GuardaCapitaldaCultura2027/Models/Context/GuardaEventosBdContext.cs
+++ GuardaCapitaldaCultura2027/Models/Context/GuardaEventosBdContext.cs
@@ -15,8 +15,9 @@ namespace GuardaCapitaldaCultura2027.Models.Context
 
         public DbSet<Evento> Eventos { get; set; }
 
-
         public DbSet<Muicipio> Muicipios { get; set; }
+
+        public DbSet<Turista> Turistas { get; set; }
 
 
 
