8be18d0ea32d882feff7ed6c568cdd088da95a86
Carolina Rei <carolinaalexandrarei@gmail.com>
2021-01-22 01:50:56 +0000

    Alteração do layout das diferentes paginas Comentado o campo descrição do modelo Reservas

--- GuardaCapitaldaCultura2027/Models/Reserva.cs
+++ GuardaCapitaldaCultura2027/Models/Reserva.cs
@@ -24,9 +24,9 @@ namespace GuardaCapitaldaCultura2027.Models
         [StringLength(20, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 20 caracteres")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Por favor, insira uma Descrição")]
-        [StringLength(500, MinimumLength = 10, ErrorMessage = "A descrição deve ter entre 10 e 500 caracteres")]
-        public string Descricao { get; set; }
+        //[Required(ErrorMessage = "Por favor, insira uma Descrição")]
+        //[StringLength(500, MinimumLength = 10, ErrorMessage = "A descrição deve ter entre 10 e 500 caracteres")]
+        //public string Descricao { get; set; }
 
         [Display(Name = "Numero de pessoas para a Reserva")]
         public int Numero_Reserva { get; set; }
